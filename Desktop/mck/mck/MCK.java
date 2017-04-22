6b511c843fc630c2675bc2f563ebaf945371ae4f
jn01674361 <joar.nykvist@gmail.com>
2017-04-22 14:04:08 +0200

    comments added

--- Desktop/mck/mck/MCK.java
+++ Desktop/mck/mck/MCK.java
@@ -26,6 +26,7 @@ public class MCK {
      * @param args the command line arguments
      */
 
+    //distaces
     public static double dStartToBox;
     public static double d2CHAIRAREA;
     public static double d1STEPBOARDAREA;
@@ -37,13 +38,16 @@ public class MCK {
     public static double dElapsedTime;
     public static long lElapsedTime;
     
+    //class containging mocap data
     public static DataContainer Data;
+
+    //matlab workspace
     public static MatlabEngine eng;
 
-    
+    //bool for knowing if the subject has reached the box zone yet or not
     public static boolean bEnteredBoxArea=false;
     
-    //declare RB enumeration
+    //declare RB enumeration, this is set in Motive when the RB's are imported
     public static String sLeftShoulder="11";
     public static String sRightShoulder="12";
     public static String sLeftElbow="8";
@@ -69,12 +73,14 @@ public class MCK {
      */
     public static void main(String[] args)throws Exception {
         
-
+        //number of arrays and array length of mocapdata objects
         int iListLength=5;
         int iNoMoCapData= 12;
         
-
+        //path to matlab files
         String sFolderPath = "C:\\Users\\PMIL\\Desktop\\jn\\MoCapExec";
+        
+        //array containing commands to run in matlab
         String[] sCommands = new String[iNoMoCapData];                
         
         //declare MatLab commands
@@ -101,7 +107,7 @@ public class MCK {
 
         for(int i =0;i<sCommands.length;i++){System.out.println(sCommands[i]);}
         
-         
+         //initiate matlab workspace
         eng = MatlabEngine.startMatlab();
 
         // add directory to matlab search path
@@ -117,39 +123,51 @@ public class MCK {
         // connect to server
         eng.eval("Client.connect");                            
         
+        //initiate datacontainer object
         Data = new DataContainer();
+        //add matlab commands
         Data.commandList=sCommands;
+        //add array length
         Data.iListLength = iListLength;
+        //start the counter
         Data.counter=0;
+        //configure distances 
         configure();  
-        
+        //set time origin
         Data.initTime = System.currentTimeMillis();
               
         
         try{            
             while(bEnteredBoxArea==false || dCompletion>dBackHome){
-             
+                //while the subject is not in the starting position and has not yet reached the box area
+
+                //check how much of the track has been completed
                 dCompletion = Completion(eng);
                 
+                //get how much time has passed and convert to seconds *10^-1
                 lElapsedTime = (System.currentTimeMillis() - Data.initTime)/100;
                 dElapsedTime=(double) lElapsedTime;
                 
                 System.out.println("completion "+String.valueOf(100*dCompletion)+"  %");
                 System.out.println("time "+String.valueOf(dElapsedTime)+"  *10^-1 s");
                 
+                //if the subject is in the chair zone, set and print the relevant data
                 if(checkChairArea()==true){
                     Movement123678();
                     System.out.println("CHAIR");
                 }
+                //subject in stepboard zone
                 else if(checkStepboardArea()==true){
                     Movement123678();
                     System.out.println("STEPBOARD");
                 }
+                //subject in box zone
                 else if(checkBoxArea()==true){
                     Movement45();
                     bEnteredBoxArea=true;
                     System.out.println("BOX");
                 }
+                //if the subject is inbetween zones, set and print the relevant data for gait
                 else{Gait();}
 
 
@@ -169,6 +187,8 @@ public class MCK {
         // close connection
         eng.disconnect();
     }
+
+    //All print methods work analogously, loop through a specific array and print the mocap data along with the time 
 
     public static void PrintShouldervNorm(){
         for(int i=0;i<Data.LeftShouldervNorm.MoCap.length;i++){
@@ -215,7 +235,7 @@ public class MCK {
     
     public static void Movement123678()throws Exception{
         
-        
+        //sets the data deemed relevant for movements in chair zone and stepboard zone
         Data.setLeftShouldervNorm(eng);
         Data.setRightShouldervNorm(eng);
         Data.setLeftHipvNorm(eng);    
@@ -225,9 +245,10 @@ public class MCK {
         Data.setLeftKneeAngl(eng);    
         Data.setRightKneeAngl(eng);              
         
+        //increase counter by 1
         Data.counter++;
-        //System.out.println("counter "+String.valueOf(Data.counter));
         
+        //print the recorded data
         PrintShouldervNorm();
         PrintHipvNorm();
         PrintKneeAngV();
@@ -235,6 +256,8 @@ public class MCK {
       
     }
     public static void Movement45()throws Exception{
+        
+        //sets the data deemed relevant for movements in the box zone
         Data.setLeftShouldervNorm(eng);
         Data.setRightShouldervNorm(eng);
         Data.setLeftHipvNorm(eng);    
@@ -248,9 +271,10 @@ public class MCK {
         Data.setLeftElbowAngl(eng);
         Data.setRightElbowAngl(eng);    
         
+        //increase counter by 1
         Data.counter++;
-        //System.out.println("counter "+String.valueOf(Data.counter));
-        
+
+        //print the recorded data
         PrintShouldervNorm();
         PrintHipvNorm();
         PrintKneeAngV();        
@@ -264,28 +288,32 @@ public class MCK {
     }
 
     public static void Gait()throws Exception{
+        
+        //set the data deemed relevant for gait
         Data.setLeftHipvNorm(eng);    
         Data.setRightHipvNorm(eng);
 
-        
+        //increase counter by 1
         Data.counter++;
-        //System.out.println("counter "+String.valueOf(Data.counter));
-        
+
+        //print the recorded data
         PrintHipvNorm();
 
     }
 
-
+    //returns true if the subject is in what is recorded as the chair zone
     public static boolean checkChairArea(){
         if(dCompletion<dCHAIRAREA){return true;}
         else{return false;}
         
     }
+    //returns true if the subject is in what is recorded as the stepboard zone
     public static boolean checkStepboardArea(){
         if(dCompletion<d2STEPBOARDAREA && dCompletion>d1STEPBOARDAREA{return true;}
         else{return false;}
         
     }
+    //returns true if the subject is in what is recorded as the box zone
     public static boolean checkBoxArea(){
         if(dCompletion>d1BOXAREA){return true;}
         else{return false;}
@@ -294,30 +322,36 @@ public class MCK {
     public static void configure()throws Exception{
         //guide for configuring the necessary distances to be able to keep track of where in the track the subject is 
 
+        //length of track from starting position to box at the end
         System.out.println("Place one marker on the floor in the middle of the two front legs of the chair, and the other just in front of the BOX at the end of track.");
         pressEnterToContinue();
         dStartToBox = GetDistanceBetweenTwoMarkers(sMeasure1, sMeasure2);
         System.out.println("Track length configured.");
 
+        //configure the end  limit of the chair area
         System.out.print("Place one marker on the floor in the middle of the two front legs of the chair, and the other where the CHAIR AREA ENDS.");
         pressEnterToContinue();
         d2CHAIRAREA = (GetDistanceBetweenTwoMarkers(sMeasure1, sMeasure2))/dStartToBox;
         System.out.println("CHAIR AREA CONFIGURED"+ String.valueOf(d2CHAIRAREA));
 
+        //configure the start limit of the stepboard area
         System.out.println("Place one marker on the floor in the middle of the two front legs of the chair, and the other where the STEPBOARD AREA BEGINS.");
         pressEnterToContinue();
         d1STEPBOARDAREA = (GetDistanceBetweenTwoMarkers(sMeasure1, sMeasure2))/dStartToBox;
         
+        //configure the end limit of the stepboard end
         System.out.println("Place one marker on the floor in the middle of the two front legs of the chair, and the other where the STEPBOARD AREA ENDS.");
         pressEnterToContinue();
         d2STEPBOARDAREA = (GetDistanceBetweenTwoMarkers(sMeasure1, sMeasure2))/dStartToBox;
         System.out.println("STEPBOARD AREA CONFIGURED"+ String.valueOf(d2STEPBOARDAREA));
 
+        //configure the start limit of the box area
         System.out.println("Place ocne marker on the floor in the middle of the two front legs of the chair, and the other where the BOX AREA BEGINS.");
         pressEnterToContinue();
         d1BOXAREA = (GetDistanceBetweenTwoMarkers(sMeasure1, sMeasure2))/dStartToBox;
         System.out.println("BOX AREA CONFIGURED" + String.valueOf(d1BOXAREA));            
         
+        //return measuring markers to starting position for measuring completion during experiment
         System.out.println("Place the measuring markers as close to the knee marker starting positions as possible. Experiment starts once you continue.");
         pressEnterToContinue();
 
@@ -339,6 +373,10 @@ public class MCK {
     }
 
     public static double GetDistanceBetweenTwoMarkers(String sMarker1, String sMarker2)throws Exception{
+
+        //returns the norm of the vector difference between two RB vectors
+        //parameters are the numbers of the two RBs as they are enumerated in Motive
+
         eng.eval("currentPos=RBPosition("+sMarker1+", Client);");
         eng.eval("endPos=RBPosition("+sMarker2+", Client);");
         eng.eval("Distance=sqrt(sum( (currentPos-endPos).^2));");
@@ -348,7 +386,9 @@ public class MCK {
     }
     
     public static double distFromStart(String sMarker) throws Exception{
-    
+        
+        //gets the distance from a specific marker to one of the measuring markers 
+
         eng.eval("Pos1 = RBPosition("+ sMeasure1+", Client);");
         eng.eval("Pos2 = RBPosition("+ sMarker+", Client);");
         eng.eval("dist=norm(Pos1-Pos2);");
@@ -363,17 +403,23 @@ public class MCK {
         /*
         *Method for getting how much of the track has been completed
         *
+        * = 0 in start position
+        * = 1 by the box at the end of the track
+        *
         *Arguments:
         *
         *eng - Matlab workspace
 
-        *Averages the position of the two feet markers
+        *Averages the position of the two knee markers
 
         */
        
+        //get distances from start for both knees 
         double dProgress1 = distFromStart(sLeftKnee);
         double dProgress2 = distFromStart(sRightKnee);
         
+        //sum and divide by 2 for average
+        //divide by track length to get fraction of completion        
         double dCompletion = (dProgress1+dProgress2)/(2.0*dStartToBox);
 
         return dCompletion;
